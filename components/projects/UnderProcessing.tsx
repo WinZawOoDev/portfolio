75a3ff74008754e5ec536052e2b2bb984e6f13c3
WinZawOoDev <winzawoo.dev@gmail.com>
2023-09-01 11:58:49 +0630

    fix layout and add animation

--- components/projects/UnderProcessing.tsx
+++ components/projects/UnderProcessing.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import {Orbitron} from 'next/font/google'
-import BugFixingIllustration from './BugFixingIllustration'
+'use client';
 
-const orbitronFont = Orbitron({subsets:["latin"], weight:"800"})
+import React from 'react'
+import { Orbitron } from 'next/font/google'
+import { motion } from 'framer-motion'
+import UnderConsruction from './UnderConstruction'
+
+const orbitronFont = Orbitron({ subsets: ["latin"], weight: "800" })
 
 export default function UnderProcessing() {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-2 place-content-center md:-mt-10 lg:-mt-5 mx-10 md:mx-auto  h-[20em] w-fit bg-[#f8f4ff] shadow-2xl rounded-3xl'>
+        <motion.div
+            initial={{ opacity: 0, x: 30 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{
+                duration: 0.4,
+                delay: 0.2,
+            }}
+            className='grid grid-cols-1 lg:grid-cols-2 place-content-center md:-mt-10 lg:-mt-5 mx-5 md:mx-auto  h-[20em] w-fit bg-[#f8f4ff] shadow-2xl rounded-3xl'
+        >
             <div className='flex items-center px-4'>
-                <h1 className={`${orbitronFont.className} text-xl lg:text-2xl text-gray-500`}>Currently processing for my showcase project 
+                <h1 className={`${orbitronFont.className} text-xl lg:text-2xl text-gray-500`}>Currently processing for my showcase project
                     <span className='mx-5 font-extrabold text-3xl'>!</span>
                 </h1>
             </div>
             <div className='hidden lg:flex items-center justify-end pl-10 w-full max-w-lg'>
-                <BugFixingIllustration />
+                <UnderConsruction />
             </div>
-        </div>
+        </motion.div>
     )
 }
